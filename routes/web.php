c80ce2d080f411414603bf0e4f434e75aa31c53f
fazvil <vildan.fazlyev@gmail.com>
2020-07-12 15:51:20 +0300

    c

--- routes/web.php
+++ routes/web.php
@@ -70,8 +70,8 @@ Route::get('/domains/{id}', function ($id) {
 Route::post('/domains/{id}/checks', function ($id) {
     $domain = DB::table('domains')->find($id);
 
-    $data = Http::get('http://www.google.com');
-    dump('ffff');
+    $data = Http::get('$domain->name')->status();
+    dump($data);
 
     /*
 
